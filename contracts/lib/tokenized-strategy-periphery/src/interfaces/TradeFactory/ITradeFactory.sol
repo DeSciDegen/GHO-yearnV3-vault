424a56a5bfb616fcb1fcb4c2907454c37a9fc77b
David Drobnak <david.drobnak26@gmail.com>
2024-01-21 17:04:04 +0100

    Revert "moved all contract files to contracts directory"
    This reverts commit 66482d276aef23623a8a089f0ded681ad430cc67.

--- contracts/lib/tokenized-strategy-periphery/src/interfaces/TradeFactory/ITradeFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.18;
-
-interface ITradeFactory {
-    function enable(address, address) external;
-
-    function disable(address, address) external;
-}
